e9da3b48ef2edf8dde6b317ef376baf6ac17fe43
johbec <johan.tk.becker@gmail.com>
2020-05-28 11:44:02 +0200

    Version 1.4 of my superawesome script

---
+++ ReposToClone.sh
@@ -0,0 +1,168 @@
+#!/usr/bin/bash
+##
+##### 1.4
+echo '###################################################################################'
+echo '###################################################################################'
+echo '>>>> TeeKaaToolKit: A git clone script of useful tools                         <<<<'
+echo '>>>> VERSION: 1.3                                                              <<<<'
+echo '>>>> AUTHOR:Brought to you by Johan BECKER a.k.a TeeKaa                        <<<<'
+echo '$$$$ Tested with zsh 5.8 (x86_64-debian-linux-gnu)                             $$$$'
+echo '$$$$ and "Oh My Zsh" https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh       $$$$'
+echo 'plugins used in Oh My ZSH:
+                                git 
+                                gitfast
+                                history-here
+                                dnf
+                                zsh-syntax-highlighting
+                                zsh-autosuggestions
+                                fzf
+                                z
+                                web-search
+                                vault
+                                urltools
+                                ssh-agent
+                                httpie
+                                command-not-found
+                                colored-man-pages
+                                colorize
+                                catimg
+                                alias-finder
+                                                                               $$$$'
+echo '###################################################################################'
+echo '###################################################################################'
+#1.4
+echo '######## USEFUL GIT REPO TO FOLLOW  https://github.com/0x90E/0x90E.github.io ######'
+echo '###################################################################################'
+git clone --recursive git@github.com:DanMcInerney/LANs.py.git
+git clone --recursive git@github.com:thomhastings/os-scripts.git
+git clone --recursive git@github.com:0x90/wifi-arsenal.git
+git clone --recursive git@github.com:0x90/wps-scripts.git
+git clone --recursive git@github.com:0x90/bluetooth-arsenal.git
+git clone --recursive git@github.com:nccgroup/BLE-Replay.git
+git clone --recursive git@github.com:nccgroup/BLESuite.git
+git clone --recursive git@github.com:merculite/BLE-Security.git
+git clone --recursive git@github.com:0x90/vpn-arsenal.git
+git clone --recursive git@github.com:0x90/ss7-arsenal.git
+git clone --recursive git@github.com:0x90/wifi-scripts.git
+git clone --recursive git@github.com:greatscottgadgets/ubertooth.git
+git clone --recursive git@github.com:rebootuser/LinEnum.git
+git clone --recursive git@github.com:0x90/kali-scripts.git
+git clone --recursive git@github.com:jeromelebel/polipo.git
+git clone --recursive git@github.com:0x90/rdp-arsenal.git
+git clone --recursive git@github.com:0x90/sip-arsenal.git
+git clone --recursive git@github.com:0x90/qualcomm-arsenal.git
+git clone --recursive git@github.com:0x90/gsm-arsenal.git
+git clone --recursive git@github.com:0x90/wpsik.git
+git clone --recursive git@github.com:CIRCL/AIL-framework.git
+git clone --recursive git@github.com:0x90/amazon-arsenal.git
+git clone --recursive git@github.com:0x90/pentest-arsenal.git
+git clone --recursive git@github.com:0x90/osint-arsenal.git
+git clone --recursive git@github.com:0x90/android-arsenal.git
+git clone --recursive git@github.com:0x90/apkx.git
+git clone --recursive git@github.com:0x90/arsenal.git
+git clone --recursive git@github.com:zdharma/hacking-private.git
+git clone --recursive git@github.com:0x90/windows-privesc-check.git
+git clone --recursive git@github.com:0x90/hackManager.git
+git clone --recursive git@github.com:0x90/openwrt-scripts.git
+git clone --recursive git@github.com:0x90/firmware-arsenal.git
+git clone --recursive git@github.com:0x90/esp-arsenal.git
+git clone --recursive git@github.com:0x90/ism-arsenal.git
+git clone --recursive git@github.com:0x90/zigbee-arsenal.git
+git clone --recursive git@github.com:Nightbringer21/fridump.git
+git clone --recursive git@github.com:0x90/frida-push.git
+git clone --recursive git@github.com:0x90/upnp-arsenal.git
+git clone --recursive git@github.com:derv82/wifite2.git
+git clone --recursive git@github.com:0x90/wpa-bruteforcer.git
+git clone --recursive git@github.com:wiire-a/pixiewps.git
+git clone --recursive git@github.com:0x90/AutoPixieWps.git
+git clone --recursive git@github.com:0x90/atheros-arsenal.git
+git clone --recursive git@github.com:DanMcInerney/wifijammer.git
+git clone --recursive git@github.com:0x90/AtEar.git
+git clone --recursive git@github.com:Oros42/IMSI-catcher.git
+git clone --recursive git@github.com:nfc-tools/libnfc.git
+git clone --recursive git@github.com:Distrotech/wireless-regdb.git
+git clone --recursive git@github.com:0x90/cve-2016-0728.git
+git clone --recursive git@github.com:0x90/hotspotd.git
+git clone --recursive git@github.com:0x90/git-scripts.git
+git clone --recursive git@github.com:hsiafan/httpdump.git
+git clone --recursive git@github.com:0x90/middler.git
+git clone --recursive git@github.com:0x90/mitmf-examples.git
+git clone --recursive git@github.com:0x90/mitm-arsenal.git
+git clone --recursive git@github.com:iphelix/dnschef.git
+git clone --recursive git@github.com:0x90/wifuzzit.git
+git clone --recursive git@github.com:0x90/wifuzz.git
+git clone --recursive git@github.com:0x90/httpscan.git
+git clone --recursive git@github.com:cea-sec/ivre.git
+git clone --recursive git@github.com:0x90/firmware-mod-kit.git
+git clone --recursive git@github.com:0x90/routerz.git
+git clone --recursive git@github.com:0x90/futurescan.git
+git clone --recursive git@github.com:0x90/powerline-arsenal.git
+git clone --recursive git@github.com:0x90/openwips-ng.git
+git clone --recursive git@github.com:0x90/fern-wifi-cracker.git
+git clone --recursive git@github.com:0x90/waidps.git
+git clone --recursive git@github.com:0x90/lan-warz.git
+git clone --recursive git@github.com:0x90/openssh-sftp-sploit.git
+git clone --recursive git@github.com:stef/wireless-radar.git
+git clone --recursive git@github.com:0x90/smbexec.git
+git clone --recursive git@github.com:0x90/modemz.git
+git clone --recursive git@github.com:0x90/tornado-ping.git
+git clone --recursive git@github.com:0x90/python-gsmmodem.git
+git clone --recursive git@github.com:0x90/CVE-2014-0160.git
+git clone --recursive git@github.com:0x90/pentest.git
+git clone --recursive git@github.com:SecureAuthCorp/impacket.git
+git clone --recursive git@github.com:x0rz/EQGRP.git
+########################## 1.0
+git clone --recursive git@github.com:0x90/wifi-arsenal.git
+git clone --recursive git@github.com:0x90/ism-arsenal.git
+git clone --recursive git@github.com:0x90/gsm-arsenal.git
+git clone --recursive git@github.com:0x90/bluetooth-arsenal.git
+git clone --recursive git@github.com:0x90/sdr-arsenal.git
+git clone --recursive git@github.com:0x90/atheros-arsenal.git
+git clone --recursive git@github.com:0x90/broadcom-arsenal.git
+git clone --recursive git@github.com:0x90/firmware-arsenal.git
+git clone --recursive git@github.com:0x90/esp-arsenal.git
+git clone --recursive git@github.com:0x90/vpn-arsenal.git
+git clone --recursive git@github.com:0x90/ss7-arsenal.git
+git clone --recursive git@github.com:0x90/nrf51-arsenal.git
+git clone --recursive git@github.com:0x90/nrf24-arsenal.git
+############### 1.1
+git clone --recursive git@github.com:sleventyeleven/linuxprivchecker.git
+git clone --recursive git@github.com:jaredhaight/scout.git
+git clone --recursive git@github.com:GhostPack/Seatbelt.git
+git clone --recursive git@github.com:carlospolop/privilege-escalation-awesome-scripts-suite.git
+git clone --recursive git@github.com:hajzer/bash-memory-dump.git
+git clone --recursive git@github.com:mzet-/linux-exploit-suggester.git
+git clone --recursive git@github.com:jondonas/linux-exploit-suggester-2.git
+git clone --recursive git@github.com:offensive-security/exploitdb.git
+############# 1.2
+echo '**********************************   DONE!!!!   ***********************************'
+echo 'IMPORTANT INFORMATION:                                                            #'
+echo 'YOU NEED TO FORK THESE REPORSITORIES IN ORDER TO CLONE THEM.                      #'
+echo 'VISIT THE LINKS, FORK THEM TO YOU ACCOUNT, THEN USE GIT CLONE ON YOUR REPOSITORY  #'
+echo '###################################################################################'
+echo '# https://github.com/wi-fi-analyzer/FrankenScript/                                #'
+echo '# https://github.com/Elycin/ReaverAutoIt/                                         #'
+echo '# https://github.com/byt3bl33d3r/pyReaver/                                        #'
+echo '# https://github.com/kurobeats/reaver_reattempt/                                  #'
+echo '# https://github.com/wi-fi-analyzer/wpspin/                                       #'
+echo '# https://github.com/Sydpy/sdp-leak-it/                                           #'
+echo '# https://github.com/AquaL1te/ssn/                                                #'
+echo '# https://github.com/hehnope/slurp/                                               #'
+echo '# https://github.com/crhua/FileScan/                                              #'
+echo '# https://github.com/decoxviii/karma/                                             #'
+echo '# https://github.com/crhua/FileScan/                                              #'
+echo '# https://github.com/nanshihui/Android-reverse-tool/                              #'
+echo '# https://github.com/wrlu/Frida-Module/                                           #'
+echo '# https://github.com/espressif/ESP8266_MESH_DEMO/                                 #'
+echo '# https://github.com/aaronkish/Atheros-AR9271/                                    #'
+echo '# https://github.com/byt3bl33d3r/mitmflib/                                        #'
+echo '# https://github.com/saironiq/mitmproxy/                                          #'
+echo '# https://github.com/byt3bl33d3r/mitmflib/                                        #'
+echo '# https://github.com/estradjm/HackRF-CUDA-Bluetooth-Sniffer/                      #'
+echo '# https://github.com/tresacton/dingdong/                                          #'
+echo '# https://github.com/armsp/NRF24L01P_rc_car/                                      #'
+echo '# https://github.com/benbiles/UHF-digital-wireless-microphone/                    #'
+echo '# https://github.com/jakubcabal/nrf24l01_fpga/                                    #'
+echo '###################################################################################'
+echo '###################################################################################'
+echo '###################################################################################'
